c4194fa3de60957215b48fcd3658cfc7535c7d3b
BRENDON ALCANTARA <09107405936@educacional.up>
2022-08-19 22:21:43 -0300

    Initial commit

---
+++ ListadePresentes/app/src/main/java/br/com/up/listadepresente/addGiftActivity.java
@@ -0,0 +1,14 @@
+package br.com.up.listadepresente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addGiftActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_gift2);
+    }
+}
